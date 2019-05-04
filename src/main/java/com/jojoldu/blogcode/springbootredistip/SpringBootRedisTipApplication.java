2c48b4a55614cdde82a98edaace7d8bb081f9197
이동욱 <jojoldu@gmail.com>
2019-05-04 20:07:31 +0900

    Initial commit

---
+++ src/main/java/com/jojoldu/blogcode/springbootredistip/SpringBootRedisTipApplication.java
@@ -0,0 +1,13 @@
+package com.jojoldu.blogcode.springbootredistip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRedisTipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRedisTipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jojoldu/blogcode/springbootredistip/SpringBootRedisTipApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jojoldu.blogcode.springbootredistip;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootRedisTipApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
