c88ce0436842ba1b8976b4dda212b202dcc40936
이동욱 <jojoldu@gmail.com>
2019-05-05 22:26:29 +0900

    레디스 도메인 추가

---
+++ src/main/java/com/jojoldu/blogcode/springbootredistip/point/AvailablePoint.java
@@ -0,0 +1,35 @@
+package com.jojoldu.blogcode.springbootredistip.point;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Getter
+@RedisHash("availablePoint")
+public class AvailablePoint implements Serializable {
+    private static final String DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+    @Id
+    private String id; // userId
+    private Long point;
+    private LocalDateTime refreshTime;
+
+    @Builder
+    public AvailablePoint(String id, Long point, LocalDateTime refreshTime) {
+        this.id = id;
+        this.point = point;
+        this.refreshTime = refreshTime;
+    }
+
+    public AvailablePoint refresh(Long point, LocalDateTime refreshTime) {
+        this.point = point;
+        this.refreshTime = refreshTime;
+        return this;
+    }
+
+
+}
---
+++ src/main/java/com/jojoldu/blogcode/springbootredistip/point/AvailablePointRedisRepository.java
@@ -0,0 +1,6 @@
+package com.jojoldu.blogcode.springbootredistip.point;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AvailablePointRedisRepository extends CrudRepository<AvailablePoint, String> {
+}
