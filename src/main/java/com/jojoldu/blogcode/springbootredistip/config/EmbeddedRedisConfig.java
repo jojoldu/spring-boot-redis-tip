539916ecefa1f1f9540f3cfd55fe04ac444b2eea
이동욱 <jojoldu@gmail.com>
2019-05-08 12:30:14 +0900

    jedis 설정 추가

---
+++ src/main/java/com/jojoldu/blogcode/springbootredistip/config/EmbeddedRedisConfig.java
@@ -0,0 +1,37 @@
+package com.jojoldu.blogcode.springbootredistip.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import redis.embedded.RedisServer;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.io.IOException;
+
+@Slf4j //lombok
+@RequiredArgsConstructor
+@Profile("local") // profile이 local일때만 활성화
+@Configuration
+public class EmbeddedRedisConfig {
+
+    private final RedisProperties redisProperties;
+
+
+    private RedisServer redisServer;
+
+    @PostConstruct
+    public void redisServer() {
+        redisServer = new RedisServer(redisProperties.getPort());
+        redisServer.start();
+    }
+
+    @PreDestroy
+    public void stopRedis() {
+        if (redisServer != null) {
+            redisServer.stop();
+        }
+    }
+}
--- src/main/java/com/jojoldu/blogcode/springbootredistip/config/RedisRepositoryConfig.java
+++ src/main/java/com/jojoldu/blogcode/springbootredistip/config/RedisRepositoryConfig.java
@@ -3,12 +3,9 @@ package com.jojoldu.blogcode.springbootredistip.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisSentinelConfiguration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
