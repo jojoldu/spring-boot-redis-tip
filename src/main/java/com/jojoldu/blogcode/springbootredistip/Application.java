f8e12bd66fb30af795dae88cd52642c8695a6176
이동욱 <jojoldu@gmail.com>
2019-05-11 11:05:11 +0900

    들여쓰기 오타 수정

--- src/main/java/com/jojoldu/blogcode/springbootredistip/Application.java
+++ src/main/java/com/jojoldu/blogcode/springbootredistip/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootRedisTipApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootRedisTipApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
