242734318523f398fcc6bff7a7e9fdb5b8f58b70
이동욱 <jojoldu@gmail.com>
2019-05-22 09:16:17 +0900

    lettuce로 변경

--- src/main/java/com/jojoldu/blogcode/springbootredistip/controller/ApiController.java
+++ src/main/java/com/jojoldu/blogcode/springbootredistip/controller/ApiController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 import java.util.SplittableRandom;
 
 @Slf4j
@@ -25,7 +26,7 @@ public class ApiController {
 
     @GetMapping("/keys")
     public String keys() {
-        redisTemplate.getConnectionFactory().getConnection().keys("*".getBytes());
+        Set<byte[]> keys = redisTemplate.keys("*");
         return "keys";
     }
 
