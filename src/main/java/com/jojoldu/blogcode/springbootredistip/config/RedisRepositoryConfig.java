20ebc165aa00b2364b11b157f6228e9545e3044a
이동욱 <jojoldu@gmail.com>
2019-05-31 09:20:32 +0900

    쓰레드 1개로 테스트

--- src/main/java/com/jojoldu/blogcode/springbootredistip/config/RedisRepositoryConfig.java
+++ src/main/java/com/jojoldu/blogcode/springbootredistip/config/RedisRepositoryConfig.java
@@ -21,35 +21,35 @@ public class RedisRepositoryConfig {
     private final RedisProperties redisProperties;
 
     // jedis
-//    @Bean
-//    public RedisConnectionFactory redisConnectionFactory() {
-//        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisProperties.getHost(), redisProperties.getPort());
-//        return new JedisConnectionFactory(config);
-//    }
-
-//    jedis connection pool
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisProperties.getHost(), redisProperties.getPort());
-        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(config);
-        jedisConnectionFactory.setPoolConfig(jedisPoolConfig());
-        return jedisConnectionFactory;
+        return new JedisConnectionFactory(config);
     }
 
-    private JedisPoolConfig jedisPoolConfig() {
-        final JedisPoolConfig poolConfig = new JedisPoolConfig();
-        poolConfig.setMaxTotal(128);
-        poolConfig.setMaxIdle(128);
-        poolConfig.setMinIdle(36);
-        poolConfig.setTestOnBorrow(true);
-        poolConfig.setTestOnReturn(true);
-        poolConfig.setTestWhileIdle(true);
-        poolConfig.setMinEvictableIdleTimeMillis(Duration.ofSeconds(60).toMillis());
-        poolConfig.setTimeBetweenEvictionRunsMillis(Duration.ofSeconds(30).toMillis());
-        poolConfig.setNumTestsPerEvictionRun(3);
-        poolConfig.setBlockWhenExhausted(true);
-        return poolConfig;
-    }
+//    jedis connection pool
+//    @Bean
+//    public RedisConnectionFactory redisConnectionFactory() {
+//        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisProperties.getHost(), redisProperties.getPort());
+//        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(config);
+//        jedisConnectionFactory.setPoolConfig(jedisPoolConfig());
+//        return jedisConnectionFactory;
+//    }
+//
+//    private JedisPoolConfig jedisPoolConfig() {
+//        final JedisPoolConfig poolConfig = new JedisPoolConfig();
+//        poolConfig.setMaxTotal(128);
+//        poolConfig.setMaxIdle(128);
+//        poolConfig.setMinIdle(36);
+//        poolConfig.setTestOnBorrow(true);
+//        poolConfig.setTestOnReturn(true);
+//        poolConfig.setTestWhileIdle(true);
+//        poolConfig.setMinEvictableIdleTimeMillis(Duration.ofSeconds(60).toMillis());
+//        poolConfig.setTimeBetweenEvictionRunsMillis(Duration.ofSeconds(30).toMillis());
+//        poolConfig.setNumTestsPerEvictionRun(3);
+//        poolConfig.setBlockWhenExhausted(true);
+//        return poolConfig;
+//    }
 
     // lettuce
 //    @Bean
